2349fb58bb5d3c0b4df26db6370913ce6441afa7
Lucas G <lucasgomes062005@gmail.com>
2025-06-17 13:24:37 -0300

    linux ta zoado, salvar pra ir pro windows

--- src/my_FPU.sv
+++ src/my_FPU.sv
@@ -25,7 +25,7 @@ module FPU(
 );
 
 state_t EA, PE;
-logic sign_A, sign_B, sign_OUT, carry, compare;
+logic sign_A, sign_B, sign_OUT, carry, compare, start, done_decode;
 logic [21:0] mant_A, mant_B, mant_TMP, mant_OUT;
 logic [9:0] exp_A, exp_B, exp_TMP, exp_OUT; // vou ter que ver quantos espaços precisa
 logic [9:0] diff_Exponent;
@@ -42,9 +42,32 @@ end
 always_comb begin
     PE = EA; // valor padrão
     case (EA)
-        DECODE:      PE = ALIGN;
-        ALIGN:       PE = OPERATE;
-        OPERATE:     PE = NORMALIZE;
+        DECODE:
+        begin
+            if(done_decode) begin    
+                PE = ALIGN; 
+            end else begin
+                PE = DECODE;
+            end
+        end
+        ALIGN:
+        begin
+            if(done_align) begin
+                PE = OPERATE;
+            end
+            else begin
+                PE = ALIGN;
+            end
+        end
+        OPERATE:   
+        begin
+            if(done_operate) begin
+                PE = NORMALIZE;
+            end
+            else begin
+                PE = OPERATE;
+            end
+        end 
         NORMALIZE:   PE = (mant_TMP[21] || exp_TMP == 0 || counter == 5'd21) ? WRITEBACK : NORMALIZE;
         WRITEBACK:   PE = DECODE;
         default:     PE = DECODE;
@@ -59,23 +82,32 @@ always_ff @(posedge clock_100Khz or negedge reset) begin
             mant_TMP   <= 22'd0;
             exp_TMP    <= 10'd0;
             counter <= 5'd0;
+            start <= 0;
+            done_decode <= 0;
     end else begin
         case(EA) 
             DECODE: begin
-                compare =  (Op_A_in[30:21] >= Op_B_in[30:21])? 1'b1 : 1'b0; // ve qual é maior ou se são iguais
-  
-                mant_A <= compare ? {1'b1,Op_A_in[20:0]} : {1'b1,Op_B_in[20:0]}; // armazena mant do maior em A
-                exp_A  <= compare ? Op_A_in[30:21] : Op_B_in[30:21];             // exp do maior em A
-                sign_A <= compare ? Op_A_in[31] : Op_B_in[31];                   // sinal do maior 
+                if(start) begin
+                    compare =  (Op_A_in[30:21] >= Op_B_in[30:21])? 1'b1 : 1'b0; // ve qual é maior ou se são iguais
+    
+                    mant_A <= compare ? {1'b1,Op_A_in[20:0]} : {1'b1,Op_B_in[20:0]}; // armazena mant do maior em A
+                    exp_A  <= compare ? Op_A_in[30:21] : Op_B_in[30:21];             // exp do maior em A
+                    sign_A <= compare ? Op_A_in[31] : Op_B_in[31];                   // sinal do maior 
 
-                mant_B <= compare ? {1'b1,Op_B_in[20:0]} : {1'b1,Op_A_in[20:0]};  // armazena mant do outro em B
-                exp_B  <= compare ? Op_B_in[30:21] : Op_A_in[30:21];              // exp do outro em B
-                sign_B <= compare ? Op_B_in[31] : Op_A_in[31];                    // sinal do outro 
-
+                    mant_B <= compare ? {1'b1,Op_B_in[20:0]} : {1'b1,Op_A_in[20:0]};  // armazena mant do outro em B
+                    exp_B  <= compare ? Op_B_in[30:21] : Op_A_in[30:21];              // exp do outro em B
+                    sign_B <= compare ? Op_B_in[31] : Op_A_in[31];                    // sinal do outro 
+                    start <= 0;
+                    done_decode <= 1;
+                end
+                else begin 
+                    start <= 1;
+                end
             end
             ALIGN: begin
                 diff_Exponent <= exp_A-exp_B; // pega a diferença dos expoentes e coloca mant_A e mant_B na mesma base, B sempre é o menor quando eles são diferentes
                 mant_B <= (mant_B >> diff_Exponent); // shift right em mant_B para alinhar
+                done_align <= 1;
             end
             OPERATE: begin
                 if (sign_A == sign_B) begin                     
@@ -92,11 +124,10 @@ always_ff @(posedge clock_100Khz or negedge reset) begin
                         exp_TMP  <= exp_TMP + 1;
                     end
                     counter <= 5'd0;
-
+                    done_operate <= 1;
             end
             NORMALIZE: begin
 
-                $display("mant_TMP=%b exp_TMP=%0d counter=%0d", mant_TMP, exp_TMP, counter);
                 if (!mant_TMP[21] && exp_TMP > 0 && counter < 21) begin   // normaliza o resultado
                         mant_TMP <= mant_TMP << 1;
                         exp_TMP  <= exp_TMP - 1;
